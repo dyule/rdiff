c752aba1b64f3b9840697f863ac6a511be00a4eb
Daniel Yule <daniel.yule@gmail.com>
2016-08-04 14:45:55 -0300

    Created docs

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rdiff"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='rdiff/struct.BlockHashes.html' title='rdiff::BlockHashes'>BlockHashes</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='rdiff/struct.Diff.html' title='rdiff::Diff'>Diff</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='rdiff/struct.Insert.html' title='rdiff::Insert'>Insert</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='rdiff/struct.Delete.html' title='rdiff::Delete'>Delete</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
